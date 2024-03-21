32709af57dff06f5bfac26703a56b29653f4955d
scf89 <scarreraf@gmail.com>
2024-03-21 19:48:03 +0100

    update

--- index.js
+++ index.js
@@ -45,3 +45,113 @@ console.log(name);
 console.log(itv1); 
 console.log(itv2); 
 console.log(itv3); 
+
+//EJERCICIO 3
+//3.1 Dado el siguiente array, crea una copia usando spread operators.
+const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsCopy = [...pointsList];
+console.log(pointsCopy);
+
+//3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyCopy = {...toy};
+console.log(toyCopy);
+
+//3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando spread operatos.
+const pointsList2 = [32, 54, 21, 64, 75, 43];
+const pointsList3 = [54,87,99,65,32];
+const combinedPoints = [...pointsList2, ...pointsList3];
+console.log(combinedPoints);
+
+//3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos con spread operators.
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']};
+const mergedToy = {...toy2, ...toyUpdate};
+console.log(mergedToy);
+
+//3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+const colorsCopy = [...colors.slice(0, 2), ...colors.slice(3)];
+console.log(colorsCopy);
+
+//EJERCICIO 4
+//4.1 Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
+const users = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+const names = users.map(user => user.name);
+console.log(names);
+
+//4.2 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+const users2 = [
+	{id: 1, name: 'Abel'},
+	{id:2, name: 'Julia'},
+	{id:3, name: 'Pedro'},
+	{id:4, name: 'Amanda'}
+];
+const modifiedNames = users2.map(user => {
+  if (user.name.startsWith('A')) {
+      return 'Anacleto';
+  } else {
+      return user.name;
+  }
+});
+console.log(modifiedNames);
+
+//4.3 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
+const cities = [
+	{isVisited:true, name: 'Tokyo'}, 
+	{isVisited:false, name: 'Madagascar'},
+	{isVisited:true, name: 'Amsterdam'}, 
+	{isVisited:false, name: 'Seul'}
+];
+const modifiedNames2 = cities.map(city => {
+  return city.isVisited ? `${city.name} (Visitado)` : city.name;
+});
+console.log(modifiedNames2);
+
+//EJERCICIO 5
+//5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean mayor que 18
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const greaterThan18 = ages.filter(age => age > 18);
+console.log(greaterThan18);
+
+//5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los valores que sean par.
+const ages2 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+const evenNumbers = ages2.filter(age => age % 2 === 0);
+console.log(evenNumbers);
+
+//5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+const lolStreamers = streamers.filter(streamer => streamer.gameMorePlayed === 'League of Legends');
+console.log(lolStreamers);
+
+//5.4 Dado el siguiente array, utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'u' en su propiedad .name. Recomendamos usar la funcion .includes() para la comprobación.
+const streamers2 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+const streamersWithU = streamers2.filter(streamer => streamer.name.includes('u'));
+console.log(streamersWithU);
+
+//5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion .includes() para la comprobación. Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando .age sea mayor que 35.
+const filteredStreamers = streamers.filter(streamer => {
+  if (streamer.gameMorePlayed.includes('Legends')) {
+      if (streamer.age > 35) {
+          streamer.gameMorePlayed = streamer.gameMorePlayed.toUpperCase();
+      }
+      return true;
+  }
+  return false;
+});
+console.log(filteredStreamers);
